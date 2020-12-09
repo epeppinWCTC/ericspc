14aa1eedb79be14e25d79983e556a8b51768198b
epeppin <epeppin@my.wctc.edu>
2020-12-08 20:01:14 -0600

    Added comments to JS and Updated fields on index.html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 
+    //function run when input changes
     $(".input-group").change(serviceChecked)
     var serviceValue = [];
     var servicePrice = [];
     var timeArray = [];
 
+
     function serviceChecked() {
+        //Collect checked items
         var checkedItems = $("input:checked");
         var serviceTotal = 0;
         var timeTotal = 0;
@@ -13,6 +16,7 @@ $(document).ready(function () {
         serviceValue = [];
         servicePrice = [];
         timeArray = [];
+        // add each item, price, and hours to arrays
         checkedItems.each(function () {
             output = "";
             serviceValue.push($(this).val());
@@ -21,14 +25,19 @@ $(document).ready(function () {
             output += (serviceValue.join(", "))
         });
 
+
+        // Calculate total price
         serviceTotal = servicePrice.reduce(function (a, b) {
             return a + b;
         }, 0)
+
+        //Calculate total time
 
         timeTotal = timeArray.reduce(function (a, b) {
             return a + b;
         }, 0)
 
+        //Display totals
         $("#options").text(output)
         $("#hours").text(`${timeTotal} hr(s)`)
         $("#total").text(`$${serviceTotal.toFixed(2)}`)
