ccb764469efb20f2b7c60f762ce8c8e7518bf267
ngorjestani <nickg2794@hotmail.com>
2020-12-09 22:27:28 -0600

    Updated trivia.js

--- js/trivia.js
+++ js/trivia.js
@@ -1,18 +1,47 @@
 $(document).ready(function () {
+    $("form div").addClass("form-check");
+    $("form div input").addClass("form-check-input");
+    $("form div label").addClass("form-check-label");
+
     var allQuestions = [$("#q0"), $("#q1"), $("#q2"), $("#q3"), $("#q4"), $("#q5"), $("#q6"), $("#q7"), $("#q8"), $("#q9")]
     var currentQuestions = [];
+    var questionsDisplay = [];
+    var numberCorrect = 0;
+    var questionsAnswered = 0;
 
     $(document.body).on("click", "#start-quiz", startQuiz);
+    $(":radio").change(answerQuestion);
 
     function startQuiz() {
         randomQuestions();
-        var questionsDisplay = [
-            allQuestions[currentQuestions[0]],
-            allQuestions[currentQuestions[0]],
-            allQuestions[currentQuestions[0]],
-            allQuestions[currentQuestions[0]],
-            allQuestions[currentQuestions[0]]
-        ];
+
+        for (var i = 0; i < currentQuestions.length; i++) {
+            questionsDisplay.push(allQuestions[currentQuestions[i]])
+        }
+
+        $("#start-quiz").hide();
+
+        questionsDisplay[0].slideToggle();
+    }
+
+    function answerQuestion() {
+        numberCorrect += $(this).data("correct");
+        questionsAnswered++;
+        if (questionsAnswered === questionsDisplay.length) {
+            endQuiz();
+        } else {
+            switchQuestions();
+        }
+    }
+
+    function switchQuestions() {
+        var currentSlide = questionsDisplay[questionsAnswered - 1];
+        currentSlide.slideToggle(showNextQuestion);
+    }
+
+    function showNextQuestion() {
+        var slideTo = questionsDisplay[questionsAnswered];
+        slideTo.slideToggle();
     }
 
     function randomQuestions() {
@@ -24,5 +53,14 @@ $(document).ready(function () {
                 currentQuestions.push(randomNumber);
             }
         }
+    }
+
+    function endQuiz() {
+        var percentage = (numberCorrect / currentQuestions.length) * 100;
+        $("#message-area h3").append(`You answered ${percentage.toFixed(0)}% correct`)
+
+        $("#message-area").show();
+
+        $("input:checked").removeAttr("checked");
     }
 });
